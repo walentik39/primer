5de0f16c9cd07ef965cabceb05ec833d729b55e0
walentik39 <107572801+walentik39@users.noreply.github.com>
2022-06-23 07:23:52 +0300

    Update pz.py
    refresh

--- pz.py
+++ pz.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 from random import randint
-N = 20
 num = []
-for i in range(N):
+size = len(num)
+count = 0
+for i in range(15):
     num.append(randint(0, 500))
 
-for i in range(N-1):
-    for j in range(N-i-1):
+for i in range(size-1):
+    for j in range(size-i-1):
         if num[j] > num[j+1]:
             num[j],num[j+1] = num[j+1], num[j]
+            count += 1
 
 print(num)  
 
