36c544cb3732eec2e92cdceff300c2928680163f
valentin <alzobavalentin@mail.ru>
2022-09-30 15:14:45 +0300

    new

--- repl.py
+++ repl.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-from random import *
+import random 
 for i in range(12):
 	i = i * "XYZ"
 	a = i.replace("X","x").split("Y")
 	print(a)
+
+
+x = 0
+while x<(random.randint(2,22)):
+    print(x)
+    x += 1
+
+print("Done")
