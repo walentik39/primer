6ed60c86f99af846681e87ba55efcc2bfeb90077
walentik39 <alzobavalentin@mail.ru>
2022-12-04 22:46:30 +0300

    two file

---
+++ req.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import grequests
+import requests
+
+class R:
+    def main_func():
+        r = requests.get('https://lubuntu.net')
+        print(f"{r.headers}-{r.status_code}")
+    main_func()
+
+
+if __name__=='__main__':
+    R
