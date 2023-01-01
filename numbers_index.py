190efc4095f99af034ab60954b69437e80c8311e
walentik39 <alzobavalentin@mail.ru>
2023-01-01 18:22:16 +0300

    n

--- numbers_index.py
+++ numbers_index.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
-numbers = [1, 8, 3, 2, 6]
-size = 5
-current_index = 0
-max_number_index = 0
-max = numbers[0]
-while (current_index < size):
-    if(numbers[current_index] > max):
-        max = numbers[current_index]
-        max_number_index = current_index
-    current_index = current_index + 1
-    print(max)
-    print(current_index)
+import random
+
+numbers = []
+for i in range(12):
+    numbers.append(random.randint(12,222))
+    print(numbers)
+    size = len(numbers)
+    current_index = 0
+    max_number_index = 0
+    max = numbers[0]
+    while (current_index < size):
+        if(numbers[current_index] > max):
+            max = numbers[current_index]
+            max_number_index = current_index
+        current_index = current_index + 1
+        print(max)
+        print(current_index)
