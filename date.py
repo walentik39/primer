c1610211ba08193d71a87edd6749ff467348ee7c
valentin <alzobavalentin@mail.ru>
2023-03-08 19:52:23 +0300

    new_date

---
+++ date.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import datetime
+my_datetime = datetime.datetime.now()
+my_date = my_datetime.strftime("%d %b")
+my_date_1 = my_datetime.strftime("%H:%M")
+print(my_date)
+print(my_date_1)
