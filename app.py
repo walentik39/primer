6a99658e3e80188a18637bf1eb273d182848012b
valentin <alzobavalentin@mail.ru>
2023-03-28 16:20:38 +0300

    edit

--- app.py
+++ app.py
@@ -14,5 +14,17 @@ tabview = ctk.CTkTabview(master=app)
 tabview.pack(pady=20, padx=50, fill='both', expand=True)
 tab_1 = tabview.add("Случайное")
 tab_2 = tabview.add("Добавить")
+# Tab 1
+label_id = ctk.CTkLabel(master=tab_1, text="1")
+label_id.pack()
+
+textbox_1 = ctk.CTkTextbox(master=tab_1, width=449, height=299, wrap="word", spacing3=10)
+textbox_1.pack()
+
+example = """Не завидуй тому, кто силен и богат,
+за рассветом всегда наступает закат.
+С этой жизнью короткою, равною вдоху,
+Обращайся, как с данной тебе напрокат."""
+textbox_1.insert("0.0", example)
 
 app.mainloop()
