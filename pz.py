1962cd4e33c1053f2562477bcd4dcf5499983a13
walentik39 <alzobavalentin@mail.ru>
2023-03-28 20:49:16 +0300

    edit pz

--- pz.py
+++ pz.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 from random import randint
-num = []
-size = len(num)
-count = 0
-for i in range(15):
-    num.append(randint(0, 500))
+with open('test.md','w') as myfile:
 
-for i in range(size-1):
-    for j in range(size-i-1):
-        if num[j] > num[j+1]:
-            num[j],num[j+1] = num[j+1], num[j]
-            count += 1
+    num = []
+    size = len(num)
+    count = 0
+    for i in range(15):
+        num.append(randint(0, 500))
+
+    for i in range(size-1):
+        for j in range(size-i-1):
+            if num[j] > num[j+1]:
+                num[j],num[j+1] = num[j+1], num[j]
+                count += 1
+                myfile.write(str(count))
 
 print(num)
 print(count)
