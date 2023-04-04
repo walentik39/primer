a3d416ce4a940e50be3c5bea5e20ba8ef46126b5
walentik39 <alzobavalentin@mail.ru>
2023-04-04 22:23:45 +0300

    edit file

--- file.py
+++ file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import os
-f = open('test.txt','a', encoding='UTF-8')
-os.system('nslookup mail.ru > test.txt')
-os.system('lsof -i > test.txt')
-f.close()
-
+import datetime
+x = datetime.datetime.now()
+with open('test.txt','a') as myfile:
+    myfile.write(str(x))
 f = open('test.txt', 'r', encoding='UTF-8')
 for x in f:
     print(x)
