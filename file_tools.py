f6c3fab08c763b624cc43d138f4337bdbd1143ee
valentin <alzobavalentin@mail.ru>
2023-04-08 23:18:09 +0300

    edit file_tools

--- file_tools.py
+++ file_tools.py
@@ -3,10 +3,13 @@ import os
 import random
 
 def f():
+    with open('file_tool.md','a') as myfile:
+        a = []
+        for i in range(7):
+            a.append(random.randint(1,222))
+            myfile.write(str(sorted(a, key=None, reverse=True)))
+
     with open('file_tool.md','r+') as mfile:        
         for j in mfile:
             print(j)
-    with open('file_tool.md','a') as myfile:
-        x = os.listdir('.')
-        myfile.write(str(x))
 f()         
