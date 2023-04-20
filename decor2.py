a54b151773e8042b18c29644f0e8e6aedec022ec
walentik39 <alzobavalentin@mail.ru>
2023-04-20 16:20:18 +0300

    decor2

---
+++ decor2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import time
+
+def my_decor(func):
+    def my_wr():
+        start_time = time.time()
+        func()
+        print(time.time() - start_time)
+    return my_wr
+
+@my_decor
+def sp():
+    spisok = [i ** 2 for i in range(100000) if i % 2 == 0]
+    print(spisok)
+
+sp()
