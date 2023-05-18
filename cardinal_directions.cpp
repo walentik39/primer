c23e7a5e0202773044956c650f5b51aad2342342
valentin <alzobavalentin@mail.ru>
2023-05-18 18:34:35 +0300

    new cpp

---
+++ cardinal_directions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <ctime>
+
+using namespace :: std;
+
+enum CardinalDirections
+{
+	North = 25,
+	South,
+	East,
+	West
+};
+int main()
+{
+	cout << "Displaying directions and their symbolic values"
+		<< endl;
+	cout << "North:"<< North << endl;
+	cout << "South:"<< South << endl;
+	cout << "East:"<< East << endl;
+	cout << "West:"<< West << endl;
+
+	CardinalDirections WindDirection = South;
+	cout << "Variable WindDirection = " << WindDirection << endl;
+	return 0;
+}
