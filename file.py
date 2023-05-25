e799f3a3effd69b4a0d835e63081add7c3c5ff42
walentik39 <alzobavalentin@mail.ru>
2023-05-25 20:36:09 +0300

    file

--- file.py
+++ file.py
@@ -5,6 +5,8 @@ import datetime
 x = datetime.datetime.now()
 with open('test.txt','a') as myfile:
     myfile.write(str(x))
+    myfile.write(str(os.system('uname -a >> test.txt')))
+
 f = open('test.txt', 'r', encoding='UTF-8')
 for x in f:
     print(x)
