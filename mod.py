f1c19c1899dcc6a47b106a88e25d85e6a7e210ba
walentik39 <alzobavalentin@mail.ru>
2023-07-08 09:04:31 +0300

    edit mod

--- mod.py
+++ mod.py
@@ -2,8 +2,11 @@
 
 import ipaddress
 import socket
+import sys
+import subprocess
+
 def two_main():
-    value = socket.gethostbyname('ipconfig.me')
+    value = socket.gethostbyname('test.me')
     def main():
         ip1 = ipaddress.ip_address(value)
         print(ip1,'\n')
@@ -11,4 +14,13 @@ def two_main():
     return main()       
 two_main()    
 
+def three_main():
+    result = subprocess.run(['ping','-c','3','8.8.8.0'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            )
+    return result
+
+res = three_main()
+print(''.join(str(res)))
 
