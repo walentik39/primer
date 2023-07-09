d2923e393f05e2c3318c8a9a7078f9459fc4887c
walentik39 <alzobavalentin@mail.ru>
2023-07-09 21:16:05 +0300

    mod2

---
+++ mod2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import ipaddress
+import socket
+import sys
+import subprocess
+
+def two_main():
+    value = socket.gethostbyname('test.me')
+    def main():
+        ip1 = ipaddress.ip_address(value)
+        print(ip1,'\n')
+        print(ip1.is_global,ip1.is_private)
+    return main()       
+
+def three_main(name):
+    result = subprocess.run(['ping','-c','3','8.8.4.4'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            )
+    return result
+
+if __name__=='__main__':
+    res = three_main(two_main())
+    print(''.join(str(res)))
+
