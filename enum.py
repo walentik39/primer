e3a6ba2e1cb65f13977233b15ce1533f948cef1f
valentin <alzobavalentin@mail.ru>
2023-07-10 22:31:18 +0300

    enum

---
+++ enum.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import time
+import random
+
+class A:
+    def main_func():
+        for n in iter(lambda: random.randrange(10), 7):
+            print(n, end='\t')
+            def inner_func():
+                for i, v in enumerate("QWEIUYY"):
+                    time.sleep(0.3)
+                    print(i, v)
+
+                for i in range(len(A:="QWERTYY")):
+                    time.sleep(0.9)
+                    print(i, A[i])
+            return inner_func()
+        return main_func
+    main_func()
+
+if __name__=='__main__':
+    A
