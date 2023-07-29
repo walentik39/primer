291d0d5d062d02a0576639f56ab637f7da15908e
walentik39 <alzobavalentin@mail.ru>
2023-07-29 15:20:52 +0300

    edit_file

--- file.py
+++ file.py
@@ -3,7 +3,7 @@
 import os
 import datetime
 x = datetime.datetime.now()
-with open('test.txt','a') as myfile:
+with open('test.txt','w') as myfile:
     myfile.write(str(x))
     myfile.write(str(os.system('uname -a >> test.txt')))
 
@@ -11,3 +11,9 @@ f = open('test.txt', 'r', encoding='UTF-8')
 for x in f:
     print(x)
 f.close()    
+def fun(name):
+    res = os.getcwd()
+    return res
+
+if __name__=='__main__':
+    print(fun(f))
