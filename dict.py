29f0bcd525c6bb699f8deb618cafec914854ad36
walentik39 <alzobavalentin@mail.ru>
2023-08-12 11:11:50 +0300

    edit_dict

--- dict.py
+++ dict.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
 import random
 import subprocess
-def f(name):
-    dict = {}
+def f():
+    dict1 = {}
     a = []
-    i = 1
-    while i < 6:
+    for i in range(12):
         a.append(random.randint(1,22))
-        a = sorted(a, key=None, reverse=True)
-        dict[i] = a
-        i += 1
-    return dict
+        dict1[i] = a
+        with open('test2.odt','a') as f:
+            f.write(str(dict1))
+
 def sub_():
-    result = subprocess.run(['lsof','-i'],stdout=subprocess.PIPE,
+    result = subprocess.run(['lsof','-o'],stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             encoding='utf-8')
     output = result.stdout + result.stderr
-    print(output)
+    return output
+
+def file_write(file):
+    with open('test2.odt','w') as f:
+        f.write(str(file))
+
+def read_file():
+    with open('test2.odt','r') as fil:
+        for i in fil:
+            print(i)
+
 if __name__=='__main__':
-    res = f(sub_())
-    print(res)
+    file_write(str(sub_()))
+    print(read_file())
 
