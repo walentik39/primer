168a2b6f438c4aa4a67480cedd4f0536b8a7a958
walentik39 <alzobavalentin@mail.ru>
2023-08-18 16:05:30 +0300

    edit srez

--- srez.py
+++ srez.py
@@ -9,17 +9,27 @@ def fun():
        print(a)
        a.append(random.randint(12,22))
        i += 1
+    return a
 
-def main_func(name):
+def main_func():
     count = 0
     for i in range(int(input())+1):
         count += i
     return count    
 
+def write_file(name):
+    with open('test.odt','w') as f:
+        f.write(str(name))
+
+def read_file():
+    with open('test.odt','r') as fi:
+        i = fi.read()
+        print(i)
+
 if __name__=='__main__':
-    result = main_func(fun())
-    print(result)
-    result2 = main_func(fun())
-    print(result2)
+    result = write_file(main_func())
+    result1 = read_file()
+    result2 = write_file(fun())
+    result3 = read_file()
 
 
