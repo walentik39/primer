6748e87837b1c002d5073c389d99ff7a30a6f30f
walentik39 <alzobavalentin@mail.ru>
2023-09-27 21:12:53 +0300

    helloy

--- helloy.c
+++ helloy.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-int main(int argc, char* argv[])
+int func(int a, int b)
 {
+
 	int x;
 	int y;
 	printf("Введём значения x и y \n");
 	scanf("%d", &x);
 	scanf("%d", &y);
-	printf("результат = %d \n", x + y);
+	fprintf(stdout,"%d\n", x + y);
+}
+
+int main(int argc, char* argv[])
+{
+	int res;
+	res = func(random(),random());
+	printf("%d %d\n",res);
 	return 0;
 }
