ce4e3ff51a00ab4ab32be55e0957e9a508095c09
walentik39 <alzobavalentin@mail.ru>
2023-10-18 07:29:08 +0300

    edit

--- din_mass2.c
+++ din_mass2.c
@@ -8,7 +8,12 @@ int main(int argc, char* argv[])
 	printf("Enter size of array to create:");
 	scanf("%d", &N);
 
-	char *A = (char*)malloc(N);;
+	char *A = (char*)malloc(N);
+	if (NULL == A)
+	{
+		printf("Os didnt't gave memory. Exit..\n");
+		exit(1);
+	}
 	for (int i =0; i < N;++i)
 		A[i] = i;
 
