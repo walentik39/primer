a8c685b791cc21a6a2b0a34b9bdbe973993b5e38
walentik39 <alzobavalentin@mail.ru>
2023-11-14 20:40:45 +0300

    mkdir pas

---
+++ pascal/prog.pas
@@ -0,0 +1,12 @@
+var
+	a,b:integer;
+	result:double;
+	begin
+		write('Введите первое число: ');
+		read(a);
+		write('введите второе число: ');
+		read(b);
+		result:=(a + b)/2;
+		writeln('среднее = ',result);
+end.
+
