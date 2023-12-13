601e871b489fea25bc26d83c7620fe606493831f
walentik39 <alzobavalentin@mail.ru>
2023-12-13 15:25:20 +0300

    edit pz

--- python/pz.py
+++ python/pz.py
@@ -7,9 +7,14 @@ class Num:
         def two_main():
             i = 0
             while i < 7:
-                with open('test.md','a') as myfile:
-                    num.append(random.randint(1,500))
-                    myfile.write(str(num))
+                if (i % 2 == 0):
+                    with open('test1.md','w') as myfile:
+                        num.append(random.randint(1,500))
+                        myfile.write(str(sorted(num, key=None,reverse=False)))
+                else:
+                    with open('test2.md','w') as file:
+                        num.append(random.randint(1,100))
+                        file.write(str(sorted(num, key=None,reverse=True)))
                 i += 1
         return two_main()
     main()
