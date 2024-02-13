a95d42075fe9b0afdf5aa5d6b6a10a9012bfc30b
walentik39 <alzobavalentin@mail.ru>
2024-02-13 14:44:04 +0300

    add repl

--- python/repl.py
+++ python/repl.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 
 import random 
-for i in range(12):
-	i = i * "XYZ"
-	a = i.replace("X","x").split("Y")
-	print(a)
-
-
-x = 0
-while x<(random.randint(2,22)):
-    print(x)
-    x += 1
-
-print("Done")
+def fun():
+    with open('text2.odt','r+') as f:
+        res = list(f.read())
+        random.shuffle(res)
+        with open('text2.odt','w') as file:
+            file.write(str(''.join(res)))
+fun()
