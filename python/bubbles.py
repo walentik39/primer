1d5be033bd71bf4b171291cc73eabb7ce6b1e7c9
walentik39 <alzobavalentin@mail.ru>
2024-03-03 21:36:18 +0300

    bubbles

--- python/bubbles.py
+++ python/bubbles.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-a = [2, 4, 7, 1, 6]
-count = 0
-for i in range(5):
-	for j in range(3):
-		if a[j] > a[j + 1]:
-			count += 1
-			a[j],a[j+1] = a[j+1], a[j]
-print(*a)
-print(count)
+
+import random
+a = []
+for j in range(random.randint(0,20)):
+    a.append(j)
+    random.shuffle(a)
+    for i in a:
+        print(a)
+
+print(sorted(a, key=None, reverse=False))
