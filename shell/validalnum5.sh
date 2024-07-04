f19580318bb4966dd6e9bea19798fcf19b37fb10
walentik39 <alzobavalentin@mail.ru>
2024-07-04 20:48:24 +0300

    add files

---
+++ shell/validalnum5.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+validAlphaNum()
+{
+	validchars="$(echo $1 | sed  's/[^+ [:digit:]]//g')"
+
+	if [ "$validchars" = "$1" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+/bin/echo -n "Enter input: "
+read input
+
+
+if ! validAlphaNum "$input" ; then
+	echo "Please enter only numbers. " >&2
+	exit 1
+else
+	echo "Input is valid. "
+fi
+exit 0
