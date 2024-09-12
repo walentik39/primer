1abe3cdb6dda8a3f2983da7e152a846d5fb71f73
walentik39 <alzobavalentin@mail.ru>
2024-09-12 13:49:48 +0300

    add files

---
+++ shell/host.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+read -p "Enter the host name: " host_name
+host $host_name
---
+++ shell/password.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+read -s -p "Enter the password: " passwd
+echo "Your password: $passwd"
