4766e913ef989a83a67c7d4ae9edd0721fc2888c
walentik39 <alzobavalentin@mail.ru>
2024-11-07 21:26:47 +0300

    add files

---
+++ python/test_sub.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+class My:
+    def fun(self):
+        ip_list = ['8.8.4.4','127.0.1.1','test.me','37.214.18.10','8.8.8.8']
+        for ip in ip_list:
+            result = subprocess.run(
+                ['ping','-c','1','-n', ip],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                encoding="utf-8"
+                 )   
+            output = result.stderr + result.stdout
+            print(output)
+        if result.returncode == 0:
+            print(f"Адрес {ip} пингуется")
+        else:
+            print(f"адрес {ip} не пингуется")
+
+                    
+
+if __name__=='__main__':
+    m = My()
+    m.fun()
