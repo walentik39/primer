621e8c5772a516badb8ebfa5881732f6e555a8a2
walentik39 <alzobavalentin@mail.ru>
2024-12-06 23:44:50 +0300

    add end edit files

--- python/vlojen_uslovie.py
+++ python/vlojen_uslovie.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
-a = int(input('Введите число '))
-if a > 10:
-    if a < 20:
-        print('Это число больше 10 , но меньше 20 ')
-else :
-    print('число равно 10 или меньше 10 ')
+import random
+import subprocess
+
+class R:
+    def fun(self):
+        #a = int(input('Введите число '))
+        a = random.randint(0,20)
+        with open('num.md','w') as f:
+            f.write(str(a))
+        if a > 10:
+            if a < 20:
+                print('Это число больше 10 , но меньше 20 ')
+        else :
+            print('число равно 10 или меньше 10 ')
+        #print(a)
+
+if __name__=='__main__':
+    r = R()
+    r.fun()
