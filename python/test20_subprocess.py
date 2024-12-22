da3b207b73ce7db00e4ebfa18616f8e9a0012f7d
walentik39 <alzobavalentin@mail.ru>
2024-12-22 11:05:59 +0300

    edit test20_subprocess.py

--- python/test20_subprocess.py
+++ python/test20_subprocess.py
@@ -2,9 +2,16 @@
 
 import subprocess
 import random
-ip_list = ['8.8.4.4','127.0.1.1','test.me','37.14.18.10','8.8.8.8']
-ip_choice = random.choice(ip_list)
-for ip in ip_choice:
-    result = subprocess.run(['ping','-c 1',ip],stderr=subprocess.DEVNULL,
-                            stdout=subprocess.PIPE,encoding='utf-8')
-    print(result.stdout)
+class My:
+    def fun(self):
+        ip_list = ['8.8.4.4','127.0.1.1','test.me','linux.org','8.8.8.8']
+        random.shuffle(ip_list)
+        for ip in ip_list:
+            result = subprocess.run(['ping','-c 1',ip],stderr=subprocess.DEVNULL,
+                                        stdout=subprocess.PIPE,encoding='utf-8')
+            return result.stdout
+
+if __name__=='__main__':
+    m = My()
+    with open('text.odt','w') as f:
+        f.write(str(m.fun()))
