fc6e04fe3534529534a070068b6f53da98fdb492
walentik39 <alzobavalentin@mail.ru>
2025-01-28 18:32:44 +0300

    edit text_trans.py

--- python/text_trans.py
+++ python/text_trans.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 
 import random
-
-def num():
-    a = []
-    b = []
-    c = int(input("Введите число: "))
-    i = 0
-    while i < c:
-        if i%2 == 0:
-            a.append(chr(random.randint(0,100)))
-        else:
-            b.append(chr(random.randint(0,100)))
-        i += 1
-    with open('rand_simv.odt','w') as f:
-        f.write(str(a))
-    with open('rand2_simv.odt','w') as f2:
-        f2.write(str(b))
-num()
+class N:
+    def num(self):
+        a = []
+        b = []
+        c = int(input("Введите число: "))
+        i = 0
+        while i < c:
+            if i%2 == 0:
+                a.append(chr(random.randint(0,100)))
+            else:
+                b.append(chr(random.randint(0,100)))
+            i += 1
+        with open('rand_simv.odt','w') as f:
+            f.write(str(a))
+        with open('rand2_simv.odt','w') as f2:
+            f2.write(str(b))
+if __name__=='__main__':
+    n = N()
+    n.num()
