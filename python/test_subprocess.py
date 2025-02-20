056161a5a511ecdfb889189aad4d98ab0b935f30
walentik39 <alzobavalentin@mail.ru>
2025-02-20 22:23:12 +0300

    edit dir files

--- python/test_subprocess.py
+++ python/test_subprocess.py
@@ -3,18 +3,12 @@
 import subprocess
 import random
 
-def fun():
-    res = subprocess.run(["netstat","-rn"],stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE,encoding='utf-8')
-    result = (res.stdout,res.stderr)
-    return random.choice(result)
-
-def file_write(name):
-    with open('test.odt','w') as f:
-        f.write(str(name))
+def func():
+    res = subprocess.run(['netstat','-rn'],stdout=subprocess.PIPE,
+                        stderr=subprocess.DEVNULL,encoding='utf-8')
+    return res.stdout
+    
 
 if __name__=='__main__':
-    file_write(fun())
-    with open('test.odt','r') as file:
-        rs = file.read()
-        print(rs)
+    with open('test.odt','w') as f:
+        f.write(str(func()))
