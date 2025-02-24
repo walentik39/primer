9fcab24770b554c5db9926c499a31dbf7f615f02
walentik39 <alzobavalentin@mail.ru>
2025-02-24 20:34:00 +0300

    edit ...

--- python/test_subprocess.py
+++ python/test_subprocess.py
@@ -5,7 +5,7 @@ import subprocess
 ip_list = ['8.8.4.4','127.0.1.1','test.me','37.214.18.10','8.8.8.8']
 for ip in ip_list:
     result = subprocess.run(
-            ['ping','-c','1','-n', ip],
+            ['ping','-c','1', ip],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             encoding="utf-8"
