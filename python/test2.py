7c1c6717ce3a675ba31f336157f8ee3b1d1c02fa
walentik39 <alzobavalentin@mail.ru>
2025-03-09 11:09:29 +0300

    edit test2.py

--- python/test2.py
+++ python/test2.py
@@ -4,23 +4,25 @@ import sys
 import subprocess
 import random
 import os
-def foo():
-    a=float(input("Введите первое число: "))
-    b=float(input("Введите второе число: "))
-    res = ("Вычисление среднего значения чисел:",(a + b) /2, "а корень из чисел",sqrt(a+b))
-    return res
+class Exp:
+    def foo(self):
+        a=float(input("Введите первое число: "))
+        b=float(input("Введите второе число: "))
+        res = ("Вычисление среднего значения чисел:",(a + b) /2, "а корень из чисел",sqrt(a+b))
+        return res
 
-def rand():
-    with open('test.odt','w') as f:
-        f.write(str(foo()))
+    def rand(self):
+        with open('test.odt','w') as f:
+            f.write(str(self.foo()))
 
-def convert():
-    a = input('Какой файл выбрали: ')
-    b = input('в какой превратить: ')
-    result = subprocess.run(['convert',a,b],stdout=subprocess.PIPE,
+    def convert(self):
+        a = input('Какой файл выбрали: ')
+        b = input('в какой превратить: ')
+        result = subprocess.run(['convert',a,b],stdout=subprocess.PIPE,
                             stderr=subprocess.DEVNULL,encoding='utf-8')
-    return result.stdout
+        return result.stdout
 
 if __name__=='__main__':
-    rand()
-    convert()
+    e = Exp()
+    e.rand()
+    e.convert()
