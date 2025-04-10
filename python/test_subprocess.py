033a09c33c6bbb0922ea7e0b1d33ee322940f193
walentik39 <alzobavalentin@mail.ru>
2025-04-10 20:38:35 +0300

    edit test_subprocess.py

--- python/test_subprocess.py
+++ python/test_subprocess.py
@@ -3,12 +3,21 @@
 import subprocess
 import random
 
-def func():
-    res = subprocess.run(['netstat','-rn'],stdout=subprocess.PIPE,
+class My:
+    def func(self):
+        res = subprocess.run(['netstat','-rn'],stdout=subprocess.PIPE,
                         stderr=subprocess.DEVNULL,encoding='utf-8')
-    return res.stdout
-    
+        return res.stdout
+
+    def funcs(self):
+        ip = ['openbsd.org','freebsd.org','rambler.ru','linux.org.ru','linux.net']
+        i = random.choice([x for x in ip])
+        result = subprocess.run(['ping','-c1',i],stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,encoding='utf-8')
+        return result.stdout + result.stderr
 
 if __name__=='__main__':
+    m = My()
     with open('test.odt','w') as f:
-        f.write(str(func()))
+        f.write(str(m.func()))
+        f.write(str(m.funcs()))
