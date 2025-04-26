f5d720d4f07b11c50dd3931fbe6262e12b089bc2
walentik39 <alzobavalentin@mail.ru>
2025-04-26 20:54:09 +0300

    edit shufl.py

--- python/shufl.py
+++ python/shufl.py
@@ -2,36 +2,35 @@
 
 import random
 import dis
+class M:
+    def read_file(self):
+        with open('test.odt','r') as fi:
+            result = fi.read()
+            print(result)
+            return result
 
-def read_file():
-    with open('test.odt','r') as fi:
-        result = fi.read()
-        print(result)
+    def one(self):
+        words = ['sky', 'storm', 'rock', 'falcon', 'forest']
+        random.shuffle(words)
+        val = ['1','2','3','4','5','6','7','8','9','0']
+        random.shuffle(val)
+        def main(self):
+            i = 0
+            while i < 6:
+                words.append(random.choice(words))
+                words.append(random.choice(val))
+                print(words)
+                with open('test.odt','w') as f:
+                    f.write(''.join(str(words * random.randint(0,9))))
+                i += 1
+            return main(self)
+        return dis.dis(self.one)
 
-def one():
-    words = ['sky', 'storm', 'rock', 'falcon', 'forest']
-    random.shuffle(words)
-    val = ['1','2','3','4','5','6','7','8','9','0']
-    random.shuffle(val)
-    def main():
-        i = 0
-        while i < 6:
-            words.append(random.choice(words))
-            words.append(random.choice(val))
-            print(words)
-            with open('test.odt','w') as f:
-                f.write(''.join(str(words * random.randint(0,9))))
-            i += 1
-    return main()
-
-def func(name):
-    bytecode = []
-    bytecode.append(dis.dis(name))
 
 
 
 if __name__=='__main__':
-    one()
-    read_file()
-    func(one)               
+    m = M()
+    m.one()
+    m.read_file()
 
