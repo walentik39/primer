6123f5ed13a5fee6c052e93ba4cc32fb6f471174
walentik39 <alzobavalentin@mail.ru>
2025-05-17 10:39:56 +0300

    add test2_1.py

---
+++ python/test2_1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from math import *
+import sys
+import subprocess
+import random
+import os
+class Exp:
+    def foo(self):
+        a=float(input("Введите первое число: "))
+        b=float(input("Введите второе число: "))
+        res = ("Вычисление среднего значения чисел:",(a + b) /2, "а корень из чисел",sqrt(a+b))
+        return res
+
+    def rand(self):
+        with open('test.odt','w') as f:
+            f.write(str(self.foo()))
+
+    def convert(self):
+        a = input('Какой файл выбрали: ')
+        b = input('в какой превратить: ')
+        result = subprocess.run(['convert',a,b],stdout=subprocess.PIPE,
+                            stderr=subprocess.DEVNULL,encoding='utf-8')
+        return result.stdout
+
+if __name__=='__main__':
+    e = Exp()
+    e.rand()
+    e.convert()
+    with open('test.odt','r') as fil:
+        r = fil.read()
+        print(r)
