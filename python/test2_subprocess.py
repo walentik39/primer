c3eabe43791668d13fa7c8a122ee0955f16f3388
walentik39 <alzobavalentin@mail.ru>
2025-06-24 14:04:04 +0300

    edit test2_subprocess.py

--- python/test2_subprocess.py
+++ python/test2_subprocess.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
 
+import random
 import subprocess
-def fun():
-    ip_list = ['0.0.0.0','127.0.1.1','test.me','mail.ru','8.8.8.8']
-    for ip in ip_list:
-        result = subprocess.run(['ping','-c','3',ip],stdout=subprocess.PIPE,
+class My:
+    def rand(self):
+        ip_list = ['12.0.0.0','127.0.1.1','test.me','mail.ru','8.4.4.8']
+        ip = random.choice(ip_list)
+        return ip
+
+    def fun(self):
+        result = subprocess.run(['ping','-c3',self.rand()],stdout=subprocess.PIPE,
                                 stderr=subprocess.DEVNULL,encoding='utf-8')
     #output = result.stderr + result.stdout
     #print(output)
         if result.returncode == 0:
-            print(f"Адрес {ip} пингуется")
+            print(f"Адрес пингуется")
         else:
-            print(f"адрес {ip} не пингуется")
+            print(f"адрес не пингуется")
 
 if __name__=='__main__':
-    fun()
+    m = My()
+    m.fun()
